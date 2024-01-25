adbe02b6e84abfb3a1e62d4fd029fd38bf2edb28
barrathtb <barrathtb@gmail.com>
2024-01-24 20:59:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import Country from './components/Country'; 
+import React from "react";
+import Country from "./components/Country";
 
 function App() {
- const countriesList = [
-  'USA',
-  'China',
-  'Japan',
-  'Germany',
-  'France',
-  'Brazil',
-  'Canada',
-  'India',
-  'Italy',
-  'Russia',
-  'South Korea',
-  'Australia',
-  'Netherlands',
-  'Spain',
-  'Mexico',
-  'Indonesia',
-  'Saudi Arabia',
-  'Turkey',
-  'United Kingdom'
-  
-];
+  const countriesList = [
+    "USA",
+    "China",
+    "Japan",
+    "Germany",
+    "France",
+    "Brazil",
+    "Canada",
+    "India",
+    "Italy",
+    "Russia",
+    "South Korea",
+    "Australia",
+    "Netherlands",
+    "Spain",
+    "Mexico",
+    "Indonesia",
+    "Saudi Arabia",
+    "Turkey",
+    "United Kingdom",
+  ];
 
   return (
     <div className="App">
