ff91aec4327b3e3ee1b92664485a5a47ead5d2e8
barrathtb <barrathtb@gmail.com>
2024-01-31 10:57:29 -0600

    updated styles and components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,118 @@
-import React from "react";
+/** @format */
+
+import React, { useState } from "react";
 import Country from "./components/Country";
+import {
+	Container,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Paper,
+	Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+	appContainer: {
+		backgroundColor: theme.palette.background.default,
+		backgroundSize: "cover",
+		position: "center",
+		color: theme.palette.text.secondary,
+		width: "100%",
+		height: "100%",
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	tableContainer: {
+		marginTop: theme.spacing(2),
+		marginBottom: theme.spacing(2),
+		maxWidth: "80%",
+		boxShadow: theme.shadows[2],
+	},
+}));
 function App() {
-  const countriesList = [
-    "USA",
-    "China",
-    "Japan",
-    "Germany",
-    "France",
-    "Brazil",
-    "Canada",
-    "India",
-    "Italy",
-    "Russia",
-    "South Korea",
-    "Australia",
-    "Netherlands",
-    "Spain",
-    "Mexico",
-    "Indonesia",
-    "Saudi Arabia",
-    "Turkey",
-    "United Kingdom",
-  ];
+	const classes = useStyles();
+	const [countriesList, setCountries] = useState([
+		{ id: 1, name: "USA", gold: 2 },
+		{ id: 2, name: "China", gold: 0 },
+		{ id: 3, name: "Japan", gold: 0 },
+		{ id: 4, name: "Germany", gold: 0 },
+		{ id: 5, name: "France", gold: 0 },
+		{ id: 6, name: "Brazil", gold: 0 },
+	]);
+	// const [selectedCountryId, setSelectedCountryId] = useState(1);
 
-  return (
-    <div className="App">
-      <Country countries={countriesList} />
-    </div>
-  );
+	// Function to decrement gold count
+	const decrementGold = (countryId) => {
+		setCountries(
+			countriesList.map((country) =>
+				country.id === countryId && country.gold > 0 ? { ...country, gold: country.gold - 1 } : country
+			)
+		);
+	};
+
+	// Function to increment gold count
+	const incrementGold = (countryId) => {
+		setCountries(
+			countriesList.map((country) => (country.id === countryId ? { ...country, gold: country.gold + 1 } : country))
+		);
+	};
+
+	// Handler for change based on selected country and increment decision
+	// const handleMedalChange = (increment) => {
+	// 	setCountries(
+	// 		countriesList.map((country) => {
+	// 			if (country.id === selectedCountryId) {
+	// 				const newGoldCount = increment ? country.gold + 1 : Math.max(country.gold - 1, 0);
+	// 				return { ...country, gold: newGoldCount };
+	// 			}
+	// 			return country;
+	// 		})
+	// 	);
+	// };
+
+	// const selectedCountry = countriesList.find((country) => country.id === selectedCountryId);
+
+	return (
+		<Container className={classes.appContainer}>
+			<h1 className={classes.title}>Gold Medals by Country</h1>
+			<Grid container spacing={3}>
+				{/* Country components showing increment and decrement buttons */}
+				{countriesList.map((country) => (
+					<Grid item xs={12} sm={6} md={4} key={country.id}>
+						<Country key={country.id} data={country} decrementGold={decrementGold} incrementGold={incrementGold} />
+					</Grid>
+				))}
+			</Grid>
+
+			{/* Medal Count Table */}
+			<TableContainer item xs={12} sm={6} md={4} className={classes.tableContainer} component={Paper}>
+				<Table aria-label='simple table'>
+					<TableHead>
+						<TableRow>
+							<TableCell>Country</TableCell>
+							<TableCell align='right'>Gold Medals</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{countriesList.map((country) => (
+							<TableRow key={country.id}>
+								<TableCell component='th' scope='row'>
+									{country.name}
+								</TableCell>
+								<TableCell align='right'>{country.gold}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Container>
+	);
 }
 
 export default App;
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,132 +1,96 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Button,
   Card,
   CardContent,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
   Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
+  
 } from "@material-ui/core";
 import EmojiEventsIcon from "@material-ui/icons/EmojiEvents";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  cardContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    backgroundColor: theme.palette.background.default,
-    color: theme.palette.text.secondary,
-  },
-  tableContainer: {
-    marginTop: theme.spacing(2),
-    maxWidth: "40%",
-    boxShadow: theme.shadows[2],
-  },
-  goldMedalIcon: {
-    verticalAlign: "middle",
-    color: theme.palette.warning.main,
-  },
-}));
+const useStyles = makeStyles((theme) => {
+	return {
+		cardContainer: {
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "center",
+			alignItems: "center",
+			marginTop: "auto",
 
-function Country() {
-  const classes = useStyles();
-  const [gold, setGold] = useState(0);
-  const [country, setCountry] = useState("");
-  const [medalTally, setMedalTally] = useState({});
+			backgroundColor: theme.palette.background.default,
+			color: theme.palette.text.secondary,
+		},
+		goldMedalIcon: {
+			verticalAlign: "middle",
+			color: theme.palette.warning.main,
+		},
+		cardContents: {
+			display: "flex",
+			flexDirection: "column",
+			justifyContent: "space-evenly",
+			alignItems: "center",
+			margin: "auto",
+		},
+		delCardBtn: {
+			marginTop: theme.spacing(1),
+			padding: theme.spacing(1.5),
+      width: "100%",
+      color: theme.palette.common.white,
+			"&:hover": {
+				backgroundColor: theme.palette.secondary.dark,
+			},
+			[theme.breakpoints.down("sm")]: {
+				width: "100%",
+			},
+		},
+    addCardBtn: {
+      marginTop: theme.spacing(1),
+			padding: theme.spacing(1.5),
+			width: "100%",
+			color: theme.palette.common.white,
+			"&:hover": {
+				backgroundColor: theme.palette.primary.dark,
+			},
+			[theme.breakpoints.down("sm")]: {
+				width: "100%",
+			},
+		},
+	};
+});
+function Country({ data, decrementGold, incrementGold }) {
+	const classes = useStyles();
 
-  const handleIncrement = () => {
-    setGold((oldGold) => oldGold + 1);
-    setMedalTally((prevTally) => ({
-      ...prevTally,
-      [country]: (prevTally[country] || 0) + 1,
-    }));
-  };
-
-  const handleCountryChange = (event) => {
-    setCountry(event.target.value);
-    setGold(medalTally[event.target.value] || 0);
-  };
-
-  return (
-    <div className={classes.cardContainer}>
-      <h1 className={classes.title}>Gold Medals by Country</h1>
-      <Card variant="outlined">
-        <CardContent>
-          <FormControl fullWidth>
-            <InputLabel id="country-select-label">Country</InputLabel>
-            <Select
-              labelId="country-select-label"
-              id="country-select"
-              value={country}
-              onChange={handleCountryChange}
-            >
-              <MenuItem value="USA">USA</MenuItem>
-              <MenuItem value="China">China</MenuItem>
-              <MenuItem value="Japan">Japan</MenuItem>
-              <MenuItem value="Germany">Germany</MenuItem>
-              <MenuItem value="France">France</MenuItem>
-              <MenuItem value="Brazil">Brazil</MenuItem>
-              <MenuItem value="Canada">Canada</MenuItem>
-              <MenuItem value="India">India</MenuItem>
-              <MenuItem value="Italy">Italy</MenuItem>
-              <MenuItem value="Russia">Russia</MenuItem>
-              <MenuItem value="South Korea">South Korea</MenuItem>
-              <MenuItem value="Australia">Australia</MenuItem>
-              <MenuItem value="Netherlands">Netherlands</MenuItem>
-              <MenuItem value="Spain">Spain</MenuItem>
-              <MenuItem value="Mexico">Mexico</MenuItem>
-              <MenuItem value="Indonesia">Indonesia</MenuItem>
-              <MenuItem value="Saudi Arabia">Saudi Arabia</MenuItem>
-              <MenuItem value="Turkey">Turkey</MenuItem>
-              <MenuItem value="United Kingdom">United Kingdom</MenuItem>
-            </Select>
-          </FormControl>
-          <Typography color="textSecondary" gutterBottom>
-            Country Name: {country}
-          </Typography>
-          <Typography variant="h5">
-            Gold Medals: {gold}{" "}
-            <EmojiEventsIcon className={classes.goldMedalIcon} />
-          </Typography>
-          <Button variant="contained" color="primary" onClick={handleIncrement}>
-            Add Gold Medal
-          </Button>
-        </CardContent>
-      </Card>
-      <TableContainer component={Paper} className={classes.tableContainer}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Country</TableCell>
-              <TableCell align="right">Gold Medals</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {Object.keys(medalTally).map((entry) => (
-              <TableRow key={entry}>
-                <TableCell component="th" scope="row">
-                  {entry}
-                </TableCell>
-                <TableCell align="right">{medalTally[entry]}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+	return (
+		<div className={classes.cardContainer}>
+			<Card variant='outlined'>
+				<CardContent className={classes.cardContents}>
+					<Typography color='textSecondary' gutterBottom>
+						Country Name: {data.name}
+					</Typography>
+					<Typography variant='h5'>
+						Gold Medals: {data.gold} <EmojiEventsIcon className={classes.goldMedalIcon} />
+					</Typography>
+					<Button
+						className={classes.addCardBtn}
+						variant='contained'
+						color='primary'
+						onClick={() => incrementGold(data.id)}>
+						Add Gold Medal
+					</Button>
+					{data.gold > 0 && (
+						<Button
+							className={classes.delCardBtn}
+							variant='contained'
+							color='secondary'
+							onClick={() => decrementGold(data.id)}>
+							Remove Gold Medal
+						</Button>
+					)}
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
 
 export default Country;
